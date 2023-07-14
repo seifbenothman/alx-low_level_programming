f07ba3cd8d47bd8015b4d79b7e1ad6db37c53388
seifbenothman <seifbenothman95@gmail.com>
2023-07-13 17:24:45 -0700

    alxproject

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,12 @@
 int main(void)
 {
     /* sizeof evaluates the size of a type */
-    printf("Size of a char: %zu byte(s)\n", sizeof(char));
-    printf("Size of an int: %zu byte(s)\n", sizeof(int));
-    printf("Size of a double: %zu byte(s)\n", sizeof(double));
-    printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-    printf("Size of a float: %zu byte(s)\n", sizeof(float));
+    printf("Size of a char:%zu byte(s)\n", sizeof(char));
+    printf("Size of an int:%zu byte(s)\n", sizeof(int));
+    printf("Size of a double:%zu byte(s)\n", sizeof(double));
+    printf("Size of a long int:%zu byte(s)\n", sizeof(long int));
+    printf("Size of a long long int:%zu byte(s)\n", sizeof(long long int));
+    printf("Size of a float:%zu byte(s)\n", sizeof(float));
 
     return 0;
 }
