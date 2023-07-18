5b29273855796f6659df0c56e528bc0431d0886e
seifbenothman <seifbenothman95@gmail.com>
2023-07-18 16:18:01 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
-#include "main.h"
-/**
- * define the _putchar function to avoid comilation errore
-*/
-int main(void) {
-char c[] = "_putchar";
-int a;
 
-for (a = 0 ; a < 8 ; a++)
-_putchar (c[a]);
-_putchar ('\n');
-return (0);
+/* Function prototype */
+int _putchar(char c);
 
+int main(void)
+{
+	char c[] = "_putchar";
+		int a;
+
+		for (a = 0; a < 8; a++)
+		_putchar(c[a]);
+
+		_putchar('\n');
+		return (0);
 }
+
+/**
+ ** _putchar - Custom function to output a character to stdout.
+ ** @c: The character to be printed.
+ **
+ ** Return: The character that was printed.
+ **/
+int _putchar(char c)
+{
+		return putchar(c);
+}
+
