3869bd57034fc079cad8b3b590f56d677e2fc78a
seifbenothman <seifbenothman95@gmail.com>
2023-07-18 16:11:08 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include "main.h"
-/* define the _putchar function to avoid comilation errore
+/**
+ * define the _putchar function to avoid comilation errore
 */
-	int _putchar(char c) {
-		return write(1, &c, 1);
-	} 
-
 	int main(void) {
 		char c[] = "_putchar";
 		int a;
