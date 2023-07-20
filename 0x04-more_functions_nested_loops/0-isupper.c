28de87ac15846920a8c6e93c216720382f6ae486
seifbenothman <seifbenothman95@gmail.com>
2023-07-20 17:18:16 +0000

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main - check the code.
+ *
+ * return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	return (1);
+	else
+	return (0);		
+}
+
