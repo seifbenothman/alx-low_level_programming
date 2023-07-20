0cf2913a00d5f004dcad590aa2673426cc223ab0
seifbenothman <seifbenothman95@gmail.com>
2023-07-20 23:13:50 +0000

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_numbers - print 0123456789 excluding 2 and 4
+ *owned by Bwave ICT
+ *Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		if (c != 2 && c != 4) // Skip printing 2 and 4
+		{
+		putchar(c + '0');
+		}
+	}
+		putchar('\n');
+}
