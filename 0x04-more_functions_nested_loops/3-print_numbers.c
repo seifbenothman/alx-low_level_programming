55f03575fcc84af474644efe6ca42b6469f4c83b
seifbenothman <seifbenothman95@gmail.com>
2023-07-20 22:44:04 +0000

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- *print_numbers - print 0123456789
- *owned by Bwave ICT
- *Return: void
+ * print_numbers - print 0123456789
+ * owned by Bwave ICT
+ * Return: void
  */
+
+
 void print_numbers(void);
 {
 	char c;
