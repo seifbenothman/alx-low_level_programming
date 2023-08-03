a09b93f4c8c58689e160b8e99ad73c0f2f8b5ad1
seifbenothman <seifbenothman95@gmail.com>
2023-08-03 01:14:12 +0000

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,11 +10,11 @@
  */
 int squrareroot(int a, int b)
 {
-		if (b * b == a)
-					return (b);
-			else if (b * b > a)
-						return (-1);
-				return (squrareroot(a, b + 1));
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (squrareroot(a, b + 1));
 }
 /**
  * _sqrt_recursion - Calculates the square root of a number using recursion.
@@ -24,5 +24,5 @@ int squrareroot(int a, int b)
  */
 int _sqrt_recursion(int n)
 {
-		return (squrareroot(n, 1));
+	return (squrareroot(n, 1));
 }
