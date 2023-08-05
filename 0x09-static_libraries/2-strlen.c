350023d58f34ce4004d5010bb9ef8428a5411f10
seifbenothman <seifbenothman95@gmail.com>
2023-08-05 00:07:29 +0000

    m

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: declaration of *s and paramters for the function _strlen
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @r: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+
+int _abs(int r)
+{
+	if (r < 0)
+		return (-r);
+	else
+		return (r);
+}
