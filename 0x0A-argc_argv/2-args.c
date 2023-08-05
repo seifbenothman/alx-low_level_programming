543d3dc24471974c1c2943ce7c8da106a375a049
seifbenothman <seifbenothman95@gmail.com>
2023-08-05 00:27:19 +0000

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments (including the program name)
+ * @argv: An array containing the command-line arguments as strings
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i < argc ; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
