be70e081f30d8a4e2b43edc5d09dde73f0a63057
seifbenothman <seifbenothman95@gmail.com>
2023-08-09 00:45:34 +0000

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Allocates a grid in memory, creates space then frees space
+ * @width: grid width
+ * @height: grid height
+ * Return: grid with deallocated spaces
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **p;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	p = malloc(height * sizeof(int *));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(sizeof(int) * width);
+		if (p[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(p[j]);
+			}
+			free(p);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
+	return (p);
+}
